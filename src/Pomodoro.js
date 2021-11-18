1db75265b3c759c94a1c66238e07755e49586a8f
Frédéric Van Overmeire <frederic.vanovermeire@gmail.com>
2021-11-18 13:46:06 +0100

    counter and css

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react"
 
 export default function Pomodoro() {
-  const [minutes, setMinutes] = useState(0)
+  const [minutes, setMinutes] = useState(5)
   const [seconds, setSeconds] = useState(2)
+  const [timer, setTimer] = useState("")
+  const [breakTime, setBreakTime] = useState ("")
   const [displayMessage, setDisplayMessage] = useState(false)
+
+  function addTime() {
+      
+  }
 
   useEffect(() => {
     let interval = setInterval(() => {
@@ -14,7 +20,7 @@ export default function Pomodoro() {
           setSeconds(59)
           setMinutes(minutes - 1)
         } else {
-          let minutes = displayMessage ? 24 : 4
+          let minutes = 0
           let seconds = 59
   
           setSeconds(seconds)
@@ -32,11 +38,25 @@ export default function Pomodoro() {
 
   return (
     <div className="pomodoro">
-      <div className="message">
-        {displayMessage && <div>Break time! New session starts in:</div>}
+
+      <div className="pomodoro__message">
+        {displayMessage && <div>Break time!</div>}
       </div>
-      <div className="timer">
+
+      <div className="pomodoro__timer">
         {timerMinutes}:{timerSeconds}
+      </div>
+
+      <div className="pomodoro__ctn-pmd">
+        <button className="pomodoro__btn pomodoro__btn--subTimer">-</button>
+        <div className="pomodoro__set-timer">{minutes}</div>
+        <button className="pomodoro__btn pomodoro__btn--addTimer">+</button>
+      </div>
+
+      <div className="pomodoro__ctn-pmd">
+        <button className="pomodoro__btn pomodoro__btn--subBreak">-</button>
+        <div className="pomodoro__set-break">5</div>
+        <button className="pomodoro__btn pomodoro__btn--addBreak">+</button>
       </div>
     </div>
   )
